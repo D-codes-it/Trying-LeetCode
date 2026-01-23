e62177258e52b78dc36b049a95eb37654f212df1
_Dina_ <bernardinasenadza880@gmail.com>
2026-01-23 00:31:18 +0000

    comments explaining changes made

--- reverse_string.py
+++ reverse_string.py
@@ -1,11 +1,13 @@
 class Solution(object):
     def reverseString(self, s):
-        
+        # REMOVED: s = ["h", "e", "l", "l", "o"] (Now it works for any input!)
         left = 0
         right = len(s) - 1
 
         while (left < right):
+            # THE SWAP (The 3-step dance) - but in Python, we can do it in one line!
             s[left], s[right] = s[right], s[left]
+            # We must move the pointers!
             left += 1
             right -= 1
         
