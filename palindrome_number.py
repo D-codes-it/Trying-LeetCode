70d94152590475113f443fc1a55fa5bbdc1ee9f0
_Dina_ <bernardinasenadza880@gmail.com>
2026-02-02 20:13:15 +0000

    attempt 1: used similar logic to reverse string (palindrome number)

--- palindrome_number.py
+++ palindrome_number.py
@@ -1,7 +1,20 @@
 class Solution(object):
+    num = int(input)
+    mum1 = map(int, input().split())
+    num2 = list(map(int, input.split()))
     def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
+        left = 0
+        right = len(x) - 1
+
+        while (left < right):
+            x[left], x[right] = x[right], x[left]
+            left += 1
+            right -= 1
+
+            if left == right:
+                print("true")
+            else:
+                print("false")
+
+#I don't think this is quite right (attempt one)
         
